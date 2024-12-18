7012733166b4ea74096b92b9728d5f0eb2feb7ba
Sucheth Shenoy <sucheth17@gmail.com>
2024-12-18 14:43:15 +0100

    Modified comments

--- example.py
+++ example.py
@@ -5,6 +5,7 @@ from cheetah import Segment, Dipole, Quadrupole, BPM, Drift, ParticleBeam
 
 def main():
 
+    # Create a cheetah segment (with one dipole and one quadrupole)
     segment = Segment(
         elements=[
             Drift(length=torch.tensor(1.0)),
@@ -23,8 +24,10 @@ def main():
     segment.QUAD1.tilt = torch.tensor(3.142e-3)
     segment.DIPOLE1.angle = torch.tensor(3.142e-2)
 
+    # Create a Segment3DPlotter instance for the segment
     plotter = Segment3DPlotter(segment)
 
+    # Generate the plot and render it
     plotter.plot_segment()
     plotter.show()
 
